d13e7df0473d90779b712834958466d7af828fcd
Admin <IDimidov@croc.ru>
2024-07-08 20:58:24 +0300

    feat: Movie

--- src/main/java/ru/croc/team4/administration/controller/SessionController.java
+++ src/main/java/ru/croc/team4/administration/controller/SessionController.java
@@ -3,13 +3,18 @@ package ru.croc.team4.administration.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.croc.team4.administration.domain.Movie;
 import ru.croc.team4.administration.domain.Session;
 import ru.croc.team4.administration.dto.SessionCreationDto;
 import ru.croc.team4.administration.dto.SessionDto;
 import ru.croc.team4.administration.mapper.SessionMapper;
+import ru.croc.team4.administration.mapper.SessionMapperImpl;
+import ru.croc.team4.administration.repository.MovieRepository;
 import ru.croc.team4.administration.repository.SessionRepository;
+import ru.croc.team4.administration.service.MovieServiceImpl;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -18,19 +23,31 @@ public class SessionController {
 
     private final SessionRepository sessionRepository;
     private final SessionMapper sessionMapper;
+    private final MovieRepository movieRepository;
 
     @Autowired
-    public SessionController(SessionRepository sessionRepository, SessionMapper sessionMapper) {
+    public SessionController(SessionRepository sessionRepository, MovieRepository movieRepository) {
         this.sessionRepository = sessionRepository;
-        this.sessionMapper = sessionMapper;
+        this.movieRepository = movieRepository;
+        this.sessionMapper = new SessionMapperImpl();
     }
 
-//    @PostMapping
-//    public ResponseEntity<SessionCreationDto> create(@RequestBody SessionCreationDto sessionDto) {
-//        var movie =
-//
-//        var session = new Session()
-//    }
+    @PostMapping
+    public ResponseEntity<SessionCreationDto> create(@RequestBody SessionCreationDto sessionDto) {
+        var movie = movieRepository
+                .findByTitle(sessionDto.movie())
+                .getTitle();
+
+        var session = new Session()
+
+        session.setMovie(sessionDto.movie());
+        session.setHall(sessionDto.hall());
+        session.setStartTime(sessionDto.startTime());
+        session.setPrice(sessionDto.price());
+        session.setMovie(movie);
+
+        return ResponseEntity.ok(sessionMapper.sessionToSessionDto(sessionRepository.save(session)));
+    }
 
     @GetMapping
     public ResponseEntity<List<SessionDto>> getSessionsBy(@RequestParam(value = "movie", required = false) String movie) {
