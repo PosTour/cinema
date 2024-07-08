400ca9b904b244fadec733f0c10a2647ca03ba47
nstepanov <nstepanov@croc.ru>
2024-07-08 16:04:28 +0300

    application and pom were added

--- src/main/java/ru/croc/team4/administration/domain/Hall.java
+++ src/main/java/ru/croc/team4/administration/domain/Hall.java
@@ -1,4 +1,35 @@
 package ru.croc.team4.administration.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@FieldNameConstants
 public class Hall {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false, length = 32)
+    private String name;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false)
+    @Convert(converter = OperationParameterConverter.class)
+    private Map<Integer, Integer> seats = new HashMap<>();
 }
