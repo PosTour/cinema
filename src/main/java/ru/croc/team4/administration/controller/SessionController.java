c5955915e4be97620a820bf7aabda830afce7d2c
posTour <dimidov496@gmail.com>
2024-07-09 00:51:27 +0300

    feat: закончил SessionController и SessionService, обновил SessionMapper и SessionRepository

--- src/main/java/ru/croc/team4/administration/controller/SessionController.java
+++ src/main/java/ru/croc/team4/administration/controller/SessionController.java
@@ -3,89 +3,54 @@ package ru.croc.team4.administration.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.croc.team4.administration.domain.Movie;
-import ru.croc.team4.administration.domain.Session;
 import ru.croc.team4.administration.dto.SessionCreationDto;
 import ru.croc.team4.administration.dto.SessionDto;
-import ru.croc.team4.administration.mapper.SessionMapper;
-import ru.croc.team4.administration.mapper.SessionMapperImpl;
-import ru.croc.team4.administration.repository.MovieRepository;
-import ru.croc.team4.administration.repository.SessionRepository;
-import ru.croc.team4.administration.service.MovieServiceImpl;
+import ru.croc.team4.administration.service.SessionServiceImpl;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("api/session")
 public class SessionController {
 
-    private final SessionRepository sessionRepository;
-    private final SessionMapper sessionMapper;
-    private final MovieRepository movieRepository;
+    private final SessionServiceImpl sessionServiceImpl;
 
     @Autowired
-    public SessionController(SessionRepository sessionRepository, MovieRepository movieRepository) {
-        this.sessionRepository = sessionRepository;
-        this.movieRepository = movieRepository;
-        this.sessionMapper = new SessionMapperImpl();
+    public SessionController(SessionServiceImpl sessionServiceImpl) {
+        this.sessionServiceImpl = sessionServiceImpl;
     }
 
     @PostMapping
-    public ResponseEntity<SessionCreationDto> create(@RequestBody SessionCreationDto sessionDto) {
-        var movie = movieRepository
-                .findByTitle(sessionDto.movie())
-                .getTitle();
-
-        var session = new Session();
-
-        session.setMovie(sessionDto.movie());
-        session.setHall(sessionDto.hall());
-        session.setStartTime(sessionDto.startTime());
-        session.setPrice(sessionDto.price());
-        session.setMovie(movie);
-
-        return ResponseEntity.ok(sessionMapper.sessionToSessionDto(sessionRepository.save(session)));
+    public ResponseEntity<SessionCreationDto> createSession(@RequestBody SessionCreationDto sessionCreationDto) {
+        SessionCreationDto sessionDto = sessionServiceImpl.createSession(sessionCreationDto);
+        return ResponseEntity.ok(sessionDto);
     }
 
     @GetMapping
     public ResponseEntity<List<SessionDto>> getSessionsBy(@RequestParam(value = "movie", required = false) String movie) {
-        var sessions = sessionRepository.findAllByMovie(movie);
+        var sessionDtos = sessionServiceImpl.getSessions(movie);
 
-        if (sessions.isEmpty()) {
+        if (sessionDtos == null) {
             return ResponseEntity.notFound().build();
         } else {
-            return ResponseEntity.ok(sessionMapper.sessionsToSessionDtos(sessions));
+            return ResponseEntity.ok(sessionDtos);
         }
     }
 
     @PutMapping
-    public ResponseEntity<SessionDto> createSession(@RequestBody SessionDto sessionDto) {
-        var sessionExistence = sessionRepository.findBySessionId(sessionDto.id());
-        if (sessionExistence.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<SessionDto> updateSession(@RequestBody SessionDto sessionDto) {
+        var session = sessionServiceImpl.updateSession(sessionDto);
 
-        var session = sessionExistence.get();
-
-        session.setMovie(sessionDto.movie());
-        session.setHall(sessionDto.hall());
-        session.setStartTime(sessionDto.startTime());
-        session.setPrice(sessionDto.price());
-
-        return ResponseEntity.ok(sessionMapper.sessionToSessionDto(sessionRepository.save(session)));
+        return session.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping
     public ResponseEntity<SessionDto> deleteSession(@RequestParam(value = "id", required = false) UUID id) {
-        var sessionExistence = sessionRepository.findBySessionId(id);
-        if (sessionExistence.isEmpty()) {
+        if (sessionServiceImpl.deleteSession(id)) {
+            return ResponseEntity.ok().build();
+        } else {
             return ResponseEntity.notFound().build();
         }
-
-        sessionRepository.delete(sessionExistence.get());
-
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ru/croc/team4/administration/mapper/SessionMapper.java
+++ src/main/java/ru/croc/team4/administration/mapper/SessionMapper.java
@@ -1,8 +1,8 @@
 package ru.croc.team4.administration.mapper;
 
 import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
 import ru.croc.team4.administration.domain.Session;
+import ru.croc.team4.administration.dto.SessionCreationDto;
 import ru.croc.team4.administration.dto.SessionDto;
 
 import java.util.List;
@@ -10,6 +10,8 @@ import java.util.List;
 @Mapper
 public interface SessionMapper {
     SessionDto sessionToSessionDto(Session session);
+
+    SessionCreationDto sessionToSessionCreationDto(Session session);
 
     List<SessionDto> sessionsToSessionDtos(List<Session> sessions);
 }
