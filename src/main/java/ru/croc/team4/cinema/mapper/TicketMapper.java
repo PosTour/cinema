55f0e7521b94be69bc2317c1cc8ac470728237f1
Nbarkhatova <Nbarkhatova@croc.ru>
2024-07-09 15:57:32 +0300

    fix: add annotation

--- src/main/java/ru/croc/team4/cinema/mapper/TicketMapper.java
+++ src/main/java/ru/croc/team4/cinema/mapper/TicketMapper.java
@@ -1,9 +1,11 @@
 package ru.croc.team4.cinema.mapper;
 
+import org.mapstruct.Mapper;
 import ru.croc.team4.cinema.domain.Ticket;
 import ru.croc.team4.cinema.dto.TicketDto;
 import java.util.List;
 
+@Mapper
 public interface TicketMapper {
     TicketDto ticketToTicketDto(Ticket ticket);
 
