cf7eef29168a891f3581598c3bfa9403be3b5f3d
AIakupov <aiakupov@croc.ru>
2024-07-09 19:29:10 +0300

    feat: создание тестов

--- src/test/java/ru/croc/team4/cinema/AdministrationApplicationTests.java
+++ src/test/java/ru/croc/team4/cinema/AdministrationApplicationTests.java
@@ -1,15 +1,37 @@
 package ru.croc.team4.cinema;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class AdministrationApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+//	@Test
+//	@DisplayName("Test get sessions by")
+	public void getSessionsByTest() throws Exception {
+		String movie = "Пираты";
+
+		mockMvc.perform(get("/api/session")
+						.param("movie", movie))
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
 }
