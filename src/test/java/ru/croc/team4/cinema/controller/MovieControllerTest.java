d21221d90088c2c71aa25c0b81ad0403e9358b39
AIakupov <aiakupov@croc.ru>
2024-07-10 18:11:11 +0300

    feat: добавлен тест создания фильма

--- src/test/java/ru/croc/team4/cinema/controller/MovieControllerTest.java
+++ src/test/java/ru/croc/team4/cinema/controller/MovieControllerTest.java
@@ -8,46 +8,33 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.testcontainers.shaded.com.google.common.reflect.TypeToken;
-import ru.croc.team4.cinema.domain.Hall;
 import ru.croc.team4.cinema.domain.Movie;
 import ru.croc.team4.cinema.dto.MovieDto;
 import ru.croc.team4.cinema.dto.MovieResponseDto;
 import ru.croc.team4.cinema.mapper.MovieMapper;
 import ru.croc.team4.cinema.mapper.MovieMapperImpl;
-import ru.croc.team4.cinema.repository.HallRepository;
 import ru.croc.team4.cinema.repository.MovieRepository;
 import ru.croc.team4.cinema.testObjects;
 import utils.ReadProperties;
 
 import java.lang.reflect.Type;
-import java.time.Duration;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -72,6 +59,8 @@ public class MovieControllerTest {
         RestAssured.port = port;
         RestAssured.useRelaxedHTTPSValidation();
 
+        if(Boolean.valueOf(String.valueOf(ReadProperties.propertiesRead().get("extended.log")))) RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+
         Movie movie = testObjects.getMovie();
         Movie movie2 = testObjects.getMovie2();
         movieRepository.save(movie);
@@ -86,35 +75,30 @@ public class MovieControllerTest {
     @Test
     @Description("Тест на создание фильма в бд")
     public void createMovieTest() throws Exception {
-
         MovieMapper movieMapper = new MovieMapperImpl();
-        MovieDto movieDto = movieMapper.movieToMovieDto(testObjects.getMovie());
+        Gson gson = new Gson();
 
-        String json = oMapper.writeValueAsString(movieDto);
-        System.out.print(json);
+        MovieDto movieDto = movieMapper.movieToMovieDto(testObjects.getMovie2());
+        String movieJson = gson.toJson(movieDto);
 
-        // нужно сделать MovieResponseDto
-        MovieResponseDto movie = MovieResponseDto.builder()
-                .id(UUID.fromString("07c9903b-f2ba-42de-84ba-21896e514f83"))
-                .title("It")
-                .durationInMinutes(122)
-                .description("gret film")
-                .build();
+        Response r = given()
+                .header("Content-Type", "application/json")
+                .body(movieJson)
+                .post("/api/movie")
+                .then()
+                .extract().response();
 
-        String result = oMapper.writeValueAsString(movie);
+        MovieResponseDto movieResponseDto = gson.fromJson(r.asString(), MovieResponseDto.class);
 
-        mockMvc.perform(post("/api/movie")
-                        .content(json)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(result))
-                .andDo(print());
+        assertAll(
+                () -> assertEquals("Пираты карибского моря", movieResponseDto.title(),
+                        "Неверное название фильма")
+        );
     }
 
     @Test
     @Description("Тест на получение корректного фильма из бд")
     public void getMovieTest() {
-
-        if(Boolean.valueOf(String.valueOf(ReadProperties.propertiesRead().get("extended.log")))) RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
         Response r = given()
                 .when()
@@ -122,6 +106,7 @@ public class MovieControllerTest {
                 .then()
                 .extract().response();
 
+        // для того чтобы определить тип json
         Type MovieResponseDtoListType = new TypeToken<List<MovieResponseDto>>() {}.getType();
 
         List<MovieResponseDto> movies = new Gson().fromJson(r.getBody().asString(), MovieResponseDtoListType);
