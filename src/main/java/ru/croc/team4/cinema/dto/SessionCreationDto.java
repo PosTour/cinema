398f1945d9ce5a54dd7bd4caff5bdc12ca0a4464
Nikita <nickit.nic098@yandex.ru>
2024-07-11 16:47:16 +0300

    fix

--- src/main/java/ru/croc/team4/cinema/dto/SessionCreationDto.java
+++ src/main/java/ru/croc/team4/cinema/dto/SessionCreationDto.java
@@ -2,9 +2,11 @@ package ru.croc.team4.cinema.dto;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import ru.croc.team4.cinema.domain.Category;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Map;
 import java.util.UUID;
 
 public record SessionCreationDto(
@@ -18,5 +20,5 @@ public record SessionCreationDto(
         @NotNull(message = "Время начала сеанса не может быть пустым")
         LocalTime startTime,
         @NotNull(message = "Цены должны быть установлены")
-        Integer prices) {
+        Map<Category,Integer> prices) {
 }
