b660162a979ab4d02c56ade2f1667cf6c2b443c3
Ilya Dimidov <145917062+PosTour@users.noreply.github.com>
2024-08-31 01:28:09 +0300

    fix: исправлен маппер места

--- src/main/java/ru/croc/team4/cinema/mapper/PlaceMapper.java
+++ src/main/java/ru/croc/team4/cinema/mapper/PlaceMapper.java
@@ -2,27 +2,17 @@ package ru.croc.team4.cinema.mapper;
 
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.croc.team4.cinema.domain.Place;
-import ru.croc.team4.cinema.domain.Row;
 import ru.croc.team4.cinema.dto.PlaceDto;
-import ru.croc.team4.cinema.repository.HallRepository;
-import ru.croc.team4.cinema.repository.PlaceRepository;
-import ru.croc.team4.cinema.repository.RowRepository;
-import ru.croc.team4.cinema.service.*;
+import ru.croc.team4.cinema.service.RowServiceImpl;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Mapper
 public interface PlaceMapper {
     @Mapping(target = "status", expression = "java(place.getStatus().lable)")
-        //TODO ckech work it or ot
-    @Mapping(target = "type", expression = "java(\"expensive\")") //TODO edit it
+    @Mapping(target = "type", expression = "java(place.getType().getRussianName())")
     @Mapping(target = "rowId", expression = "java(place.getRow().getId())")
     PlaceDto placeToPlaceDto(Place place);
 
@@ -31,13 +21,3 @@ public interface PlaceMapper {
     Place placeDtoToPlace(PlaceDto placeDto, @Context RowServiceImpl rowServiceImpl);
     List<PlaceDto> placeListToPlaceDtoList(List<Place> placeList);
 }
-
-//@Mappings({
-//        @Mapping(target = "client", expression = "java(clientRepository.findById(bookingRequest.getClientId()).orElse(null))"),        @Mapping(target = "employee",expression = "java(doctorRepository.findById(bookingRequest.getDoctorId()).orElse(null))"),
-//        @Mapping(target = "startTime", expression = "java(bookingRequest.getStartTime())"),        @Mapping(target = "endTime", expression = "java(bookingRequest.getEndTime())")
-//})Reception toReceptionBookingRequest(ReceptionBookingRequest bookingRequest,
-//                                      @Context ClientRepository clientRepository,                                    @Context DoctorRepository doctorRepository
-//);
-//default String getFullName(Reception reception){
-//    Doctor doctor = reception.getEmployee();    return doctor.getSurname() + " " + doctor.getName()
-//            + " " + doctor.getPatronymic();}
